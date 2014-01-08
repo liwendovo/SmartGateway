82981d54c22ff04b761960a2f87418295b981f0d
liwendong <liwendong@qq.com>
2014-01-08 17:22:59 +0800

    添加TUTKService及Client类

---
+++ src/com/seuic/net/TUTKService.java
@@ -0,0 +1,34 @@
+package com.seuic.net;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class TUTKService extends Service{
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+	}
+
+	@Override
+	public void onStart(Intent intent, int startId) {
+		// TODO Auto-generated method stub
+		super.onStart(intent, startId);
+	}
+	
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+
+}
